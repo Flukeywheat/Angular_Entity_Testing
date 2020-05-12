0cbc4efcc6b51ff5f25495c4144dd9b8d9c17133
flukeywheat <Chapter_120@yahoo.com>
2020-05-12 16:46:56 -0500

    Add project files.

---
+++ AspNetCoreDemo/Migrations/20200512202525_Initial Migration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace AspNetCoreDemo.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    userId = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserName = table.Column<string>(type: "varchar (100)", nullable: false),
+                    Email = table.Column<string>(type: "varchar (100)", nullable: false),
+                    Password = table.Column<string>(type: "varchar (100)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.userId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clients");
+        }
+    }
+}
